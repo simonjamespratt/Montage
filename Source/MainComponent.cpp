51f08458435049967eb5c6d4391702eaa56ef99c
Simon Pratt <simonjamespratt@gmail.com>
2019-02-24 17:58:42 +0000

    basic audio player

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,32 +1,33 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #include "MainComponent.h"
 
-//==============================================================================
-MainComponent::MainComponent()
+// constructor
+MainComponent::MainComponent() : state(Stopped)
 {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize (800, 600);
+    // Initialise display
+    addAndMakeVisible(&openButton);
+    openButton.setButtonText("Open");
+    openButton.onClick = [this] { openButtonClicked(); };
 
-    // Some platforms require permissions to open input channels so request that here
-    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
-        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
-    {
-        RuntimePermissions::request (RuntimePermissions::recordAudio,
-                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
-    }
-    else
-    {
-        // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
-    }
+    addAndMakeVisible(&playButton);
+    playButton.setButtonText("Play");
+    playButton.setColour (TextButton::buttonColourId, Colours::green);
+    playButton.onClick = [this] { playButtonClicked(); };
+    playButton.setEnabled (false);
+
+    addAndMakeVisible (&stopButton);
+    stopButton.setButtonText ("Stop");
+    stopButton.setColour (TextButton::buttonColourId, Colours::red);
+    stopButton.onClick = [this] { stopButtonClicked(); };
+    stopButton.setEnabled (false);
+
+    // Set the size of the component after you add any child components
+    setSize (300, 200);
+
+    // Initialise audio handling
+    formatManager.registerBasicFormats();
+    transportSource.addChangeListener(this);
+
+    setAudioChannels (0, 2);
 }
 
 MainComponent::~MainComponent()
@@ -45,17 +46,20 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
     // but be careful - it will be called on the audio thread, not the GUI thread.
 
     // For more details, see the help for AudioProcessor::prepareToPlay()
+    transportSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
 {
     // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    if (readerSource.get() == nullptr)
+    {
+        // Right now we are not producing any data, in which case we need to clear the buffer
+        // (to prevent the output of random noise)
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+    transportSource.getNextAudioBlock (bufferToFill);
 }
 
 void MainComponent::releaseResources()
@@ -64,13 +68,81 @@ void MainComponent::releaseResources()
     // restarted due to a setting change.
 
     // For more details, see the help for AudioProcessor::releaseResources()
+    transportSource.releaseResources();
+}
+
+// state and event handlers
+void MainComponent::changeState (TransportState newState)
+{
+    if (state != newState)
+    {
+        state = newState;
+        switch (state)
+        {
+            case Stopped:                           // [3]
+                stopButton.setEnabled (false);
+                playButton.setEnabled (true);
+                transportSource.setPosition (0.0);
+                break;
+            case Starting:                          // [4]
+                playButton.setEnabled (false);
+                transportSource.start();
+                break;
+            case Playing:                           // [5]
+                stopButton.setEnabled (true);
+                break;
+            case Stopping:                          // [6]
+                transportSource.stop();
+                break;
+        }
+    }
+}
+
+// this is the callback that is used when the transportSource change listener event occurs
+void MainComponent::changeListenerCallback (ChangeBroadcaster* source)
+{
+    if (source == &transportSource)
+    {
+        if (transportSource.isPlaying())
+            changeState (Playing);
+        else
+            changeState (Stopped);
+    }
+}
+
+void MainComponent::openButtonClicked()
+{
+    FileChooser chooser("Select file to play", {}, "*.wav,*.aif, *.aiff");
+
+    if (chooser.browseForFileToOpen()) {
+        auto file = chooser.getResult();
+        auto* reader = formatManager.createReaderFor(file);
+
+        if (reader != nullptr) {
+            std::unique_ptr<AudioFormatReaderSource> newSource (new AudioFormatReaderSource (reader, true));
+            transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);
+            playButton.setEnabled (true);
+            readerSource.reset (newSource.release());
+        }
+    }
+}
+
+void MainComponent::playButtonClicked()
+{
+    changeState(Starting);
+}
+
+void MainComponent::stopButtonClicked()
+{
+    changeState(Stopping);
 }
 
 //==============================================================================
 void MainComponent::paint (Graphics& g)
 {
+    // NB: MAY NOT NEED THIS IF ALL COMPONENTS ARE ADDED AS CHILD COMPONENTS...
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    // g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
     // You can add your drawing code here!
 }
@@ -80,4 +152,7 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    openButton.setBounds (10, 10, getWidth() - 20, 20);
+    playButton.setBounds (10, 40, getWidth() - 20, 20);
+    stopButton.setBounds (10, 70, getWidth() - 20, 20);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,40 +1,51 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-//==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public AudioAppComponent
+class MainComponent   : public AudioAppComponent, public ChangeListener
 {
 public:
-    //==============================================================================
+    // constructor and deconstructor
     MainComponent();
     ~MainComponent();
 
-    //==============================================================================
+    // AudioSource overrides
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
-    //==============================================================================
+    // AudioTransportSource (ChangeBroadaster) override
+    void changeListenerCallback (ChangeBroadcaster* source) override;
+
+    // GUI Component overrides
     void paint (Graphics& g) override;
     void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+    enum TransportState {
+         Stopped,
+        Starting,
+        Playing,
+        Stopping
+    };
+    TransportState state;
 
+    TextButton openButton;
+    TextButton playButton;
+    TextButton stopButton;
+
+    AudioFormatManager formatManager;
+    std::unique_ptr<AudioFormatReaderSource> readerSource;
+    AudioTransportSource transportSource;
+
+    void changeState (TransportState newState);
+    void openButtonClicked();
+    void playButtonClicked();
+    void stopButtonClicked();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
