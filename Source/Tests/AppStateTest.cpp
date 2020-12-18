0a95ccba15f0f798109ac9b98342f05dc4ddd280
Simon Pratt <simonjamespratt@gmail.com>
2020-12-18 17:41:31 +0000

    wip

--- Source/Tests/AppStateTest.cpp
+++ Source/Tests/AppStateTest.cpp
@@ -1,6 +1,14 @@
 #include <catch2/catch.hpp>
+#include <juce_core/juce_core.h>
 
 TEST_CASE("Creating a file")
 {
-    REQUIRE(CURRENT_BINARY_DIRECTORY == "");
+    juce::String filepath(CURRENT_BINARY_DIRECTORY);
+    filepath += "/testfile.txt";
+    juce::File file(filepath);
+    REQUIRE(file.existsAsFile() == false);
+    file.create();
+    REQUIRE(file.existsAsFile() == true);
+    file.deleteFile();
+    REQUIRE(file.existsAsFile() == false);
 }
